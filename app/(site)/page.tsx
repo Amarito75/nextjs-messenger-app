9afdbf6beb51c0c3cedc0e9ea341530aac69476c
Amarito75 <a.matallah767@gmail.com>
2023-08-01 19:37:24 +0200

    :tada: initialize project with form ui

---
+++ app/(site)/page.tsx
@@ -0,0 +1,23 @@
+import Image from "next/image";
+import React from "react";
+import AuthForm from "./components/AuthForm";
+
+export default function Home() {
+  return (
+    <div className="flex min-h-full flex-col justify-center py-12 sm:px-6 lg:px-8 bg-white">
+      <div className="sm:mx-auto sm:w-full sm:max-w-md">
+        <Image
+          alt="logo"
+          height={48}
+          width={48}
+          className="mx-auto w-auto"
+          src="/images/logo.png"
+        />
+        <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
+          Sign in to your account
+        </h2>
+        <AuthForm />
+      </div>
+    </div>
+  );
+}
